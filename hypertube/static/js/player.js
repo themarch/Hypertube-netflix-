0cb4f8fdf35669dadfd756ae856d0d05b2b0cd05
themarch <theodesconta@gmail.com>
2019-12-18 19:22:28 +0100

    hypertube_netflix completed

--- hypertube/static/js/player.js
+++ hypertube/static/js/player.js
@@ -1,24 +1,23 @@
 document.getElementById("watch").addEventListener("click", function(event){
-        document.querySelector(".image_video").style = 'filter:grayscale(1);'
-        event.stopPropagation();
-        var resp = {
-            'pal': 'pal'
-        };
-        $.ajax({
-          type: 'POST',
-          url: `${window.location}`,
-          data: {csrfmiddlewaretoken: window.CSRF_TOKEN, 'pal' : 'pal'},
-          success: function(data){
-            console.log('huhuhuh')
-            console.log(data['infos']['ids'])
-            bouboule = window.location.pathname
-            bouboule = bouboule.split('/')
-            bouboule = bouboule[2]
-            add = 'http://localhost:3000/play?film=' + data['infos']['ids']+ '&lang=' + data['infos']['lan'] + '&tok=' + data['infos']['token']
-            document.location.href=add
-            console.log(bouboule)
-          }
-        });
-    
-})
+  document.querySelector(".image_video").style = 'filter:grayscale(1);'
+  event.stopPropagation();
+  var resp = {
+      'pal': 'pal'
+  };
+  $.ajax({
+    type: 'POST',
+    url: `${window.location}`,
+    data: {csrfmiddlewaretoken: window.CSRF_TOKEN, 'pal' : 'pal'},
+    success: function(data){
+      console.log('huhuhuh')
+      console.log(data['infos']['ids'])
+      bouboule = window.location.pathname
+      bouboule = bouboule.split('/')
+      bouboule = bouboule[2]
+      add = 'http://localhost:3000/play?film=' + data['infos']['ids']+ '&lang=' + data['infos']['lan'] + '&tok=' + data['infos']['token']
+      document.location.href=add
+      console.log(bouboule)
+    }
+  });
 
+})
